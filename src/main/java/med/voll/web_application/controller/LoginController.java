4218be98afbfcf050313d878348d9353aaee5fef
Marcelo <mdbezerra16@gmail.com>
2025-05-06 13:48:35 -0300

    feat: :sparkles: Configuração do logout

--- src/main/java/med/voll/web_application/controller/LoginController.java
+++ src/main/java/med/voll/web_application/controller/LoginController.java
@@ -2,13 +2,11 @@ package med.voll.web_application.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class LoginController {
     
-    @GetMapping
+    @GetMapping("/login")
     public String carregaPaginaListagem() {
         return "autenticacao/login";
     }
--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -2,22 +2,40 @@ package med.voll.web_application.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ConfiguracoesSeguranca {
-    
+
     @Bean
     public UserDetailsService dadosUsuariosCadastrados() {
         UserDetails usuario = User.builder()
-        .username("Marcelo")
-        .password("{noop}123")
-        .build();
+                .username("marcelo@email.com")
+                .password("{noop}123")
+                .build();
 
         return new InMemoryUserDetailsManager(usuario);
+    }
+
+    @Bean
+    public SecurityFilterChain filtrosDeSeguranca(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/css/**", "/js/**", "/assets/**").permitAll();
+                    req.anyRequest().authenticated();
+                })
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll())
+                .build();
     }
 }
