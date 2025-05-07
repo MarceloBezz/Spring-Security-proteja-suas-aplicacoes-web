6c24316ccc812a8b506d00def644af691f5fdaa9
Marcelo <mdbezerra16@gmail.com>
2025-05-07 11:24:57 -0300

    feat: :sparkles: Implementa entidade Usuario e repositório, adiciona serviço de autenticação e configurações de segurança

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -4,24 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ConfiguracoesSeguranca {
-
-    @Bean
-    public UserDetailsService dadosUsuariosCadastrados() {
-        UserDetails usuario = User.builder()
-                .username("marcelo@email.com")
-                .password("{noop}123")
-                .build();
-
-        return new InMemoryUserDetailsManager(usuario);
-    }
 
     @Bean
     public SecurityFilterChain filtrosDeSeguranca(HttpSecurity http) throws Exception {
@@ -43,5 +31,10 @@ public class ConfiguracoesSeguranca {
                         .alwaysRemember(true))
                 .csrf(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder codificadorDeSenha() {
+        return new BCryptPasswordEncoder();
     }
 }
